246b6c15154cfbaed3ca023efb323b0d1264b5a7
Alfredo <marianos_nanox@hotmail.com>
2025-09-11 15:25:52 -0300

    Actividad Numero 29 Terminada

---
+++ TP29-Sumbaino_Mariano_Alfredo/helloworldtp/component/helloworld.jsx
@@ -0,0 +1,10 @@
+
+export default function Wold({ name }) {
+    
+    return ( 
+    <>
+        <h1>Hola, {name}!</h1>
+
+    </>
+    );
+}
---
+++ TP29-Sumbaino_Mariano_Alfredo/helloworldtp/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Wold from '../component/helloworld.jsx'
+function App() {
+
+  return (
+    <>
+      <Wold name="Mariano"/>
+    </>
+  )
+}
+
+export default App
